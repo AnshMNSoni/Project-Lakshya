e79aab8fdf092e0ee6b7bd3d957367228bd64244
AnshMNSoni <anshsoni702@gmail.com>
2025-09-10 19:11:25 +0530

    added api key into the .env

--- src/pages/Quiz2.tsx
+++ src/pages/Quiz2.tsx
@@ -75,7 +75,7 @@ const Quiz2 = () => {
 
   const handleCompleteQuiz = async () => {
     try {
-      const genAI = new GoogleGenerativeAI("AIzaSyCjQc6qKXraymAtLIkeYlOXayJzVmQm8pE");
+      const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
       const prompt = `
